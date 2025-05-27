d723889c17f1928c1e410bd8e3764bcdb03f7322
Junsu Jeon <pds.jeon@gmail.com>
2025-05-27 23:41:22 +0900

    Update Program.cs

--- Source/ABMGS.Server/Program.cs
+++ Source/ABMGS.Server/Program.cs
@@ -1,25 +1,15 @@
+
+using Orleans.Runtime;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Host.UseOrleans(static siloBuilder =>
+{
+    siloBuilder.UseLocalhostClustering();
+    siloBuilder.AddMemoryGrainStorage("urls");
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-app.UseAuthentication();
-
-
-
+app.Map("/", () => "Hello World");
 app.Run();
-
