17633c903c5274585f66135297acc9aeec70c217
Junsu Jeon <pds.jeon@gmail.com>
2025-07-11 10:29:28 +0900

    Added basic skeleton

---
+++ Source/ABMGS.Server.Front/Controllers/WebsocketController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
+
+namespace ABMGS.Server.Front.Controllers;
+
+[ApiController]
+[Route("/ws")]
+
+public class WebsocketController : ControllerBase
+{
+    private readonly ILogger<WebsocketController> _logger;
+
+    public WebsocketController(ILogger<WebsocketController> logger)
+    {
+        _logger = logger;
+    }
+
+    public ActionResult Get()
+    {
+        return Ok();
+    }
+}
