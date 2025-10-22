14ce4bb060c6bf735395744893aebbd5e03244bf
Junsu Jeon <pds.jeon@gmail.com>
2025-10-22 17:29:57 +0900

    Create GameSessionController.cs

---
+++ Source/ABMGS.ServerV2/Controllers/GameSessionController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ABMGS.ServerV2.Controllers;
+
+public class GameSessionController : ControllerBase
+{
+    private readonly ILogger<GameSessionController> _logger;
+    public GameSessionController(ILogger<GameSessionController> logger)
+    {
+        _logger = logger;
+    }
+
+
+}
