ef8ec6efb5efce8bc72fcbb188d88d0c9f70bcdb
Junsu Jeon <dec04th@hotmail.com>
2025-11-04 20:47:04 +0900

    Pipeline for network receiving.

--- Source/ABMGS.ServerV2/Grains/PlayerActor.cs
+++ Source/ABMGS.ServerV2/Grains/PlayerActor.cs
@@ -1,6 +1,8 @@
 using ABMGS.ServerV2.Enums;
+using System.Buffers;
 using System.IO.Pipelines;
 using System.Net.WebSockets;
+using System.Runtime.CompilerServices;
 
 namespace ABMGS.ServerV2.Grains;
 
@@ -8,6 +10,37 @@ public interface IPlayerActor : IGrainWithGuidKey
 {
     public Task StartGameLoop(WebSocket SocketHandle, string UniquePlayerId, CancellationToken AbnormalExitToken);
     // public Task<INetworkReceiveActor> GetNetworkReceiveActor();
+}
+
+public class NetworkBuffer : IDisposable
+{
+    private readonly Pipe _pipe;
+    private readonly int _bufferSize;
+
+    public NetworkBuffer(int bufferSize)
+    {
+        _pipe = new Pipe();
+        _bufferSize = bufferSize;
+    }
+
+    public Memory<byte> GetReceiveBuffer() => _pipe.Writer.GetMemory(_bufferSize);
+    public void AddBuffer(int receivedByteCount) => _pipe.Writer.Advance(receivedByteCount);
+    public async Task FinishReceived()
+    {
+        FlushResult result = await _pipe.Writer.FlushAsync();
+        await _pipe.Writer.CompleteAsync();
+    }
+
+    public async Task<byte[]> Read()
+    {
+        ReadResult readResult = await _pipe.Reader.ReadAsync();
+        return readResult.Buffer.ToArray<byte>();
+
+    }
+    public void Dispose()
+    {
+        _pipe.Reset();
+    }
 }
 public class PlayerActor : Grain, IPlayerActor
 {
@@ -26,30 +59,28 @@ public class PlayerActor : Grain, IPlayerActor
 
         bool IsGameLoopValid = true;    
 
-        Pipe pipe = new Pipe();
-        PipeWriter writer = pipe.Writer;
-
-
-        //Loop to receive data from the WebSocket
-        while(IsGameLoopValid)
+        using (NetworkBuffer NBuf = new NetworkBuffer(4096))
         {
-            while(true)
+            //Loop to receive data from the WebSocket
+            while (IsGameLoopValid && !AbnormalExitToken.IsCancellationRequested)
             {
-                Memory<byte> receiveBuffer = writer.GetMemory(4096);
-
-                ValueWebSocketReceiveResult result = await SocketHandle.ReceiveAsync(receiveBuffer, AbnormalExitToken);
-                writer.Advance(result.Count);
-                if(result.EndOfMessage == true)
+                while (true)
                 {
-                    await writer.CompleteAsync();
-                    break;
+                    ValueWebSocketReceiveResult result = await SocketHandle.ReceiveAsync(NBuf.GetReceiveBuffer(), AbnormalExitToken);
+                    NBuf.AddBuffer(result.Count);
+
+                    if (result.EndOfMessage == true)
+                    {
+                        NBuf.FinishReceived();
+                        break;
+                    }
                 }
             }
         }
     }
     public Task<INetworkReceiveActor> GetNetworkReceiveActor()
     {
-        string NetworkReceiveActorId = string.Join("/", this.GetGrainId().GetGuidKey().ToString(), ActorSuffixNames.NetworkReceiveActor);
+        string NetworkReceiveActorId = string.Join("/", this.GetGrainId().GetGuidKey().ToString(), ActorSuffixNames.NetworkReceiveActor.ToString());
         return Task.FromResult(GrainFactory.GetGrain<INetworkReceiveActor>(NetworkReceiveActorId));
     }
 
