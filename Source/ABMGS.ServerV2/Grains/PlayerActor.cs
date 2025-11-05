b83227bda480957638f20c620285be3c453564ad
Junsu Jeon <dec04th@hotmail.com>
2025-11-05 22:38:00 +0900

    GameSessionActer added

--- Source/ABMGS.ServerV2/Grains/PlayerActor.cs
+++ Source/ABMGS.ServerV2/Grains/PlayerActor.cs
@@ -7,13 +7,6 @@ using SyncnetPlatform.Dto;
 using System.Runtime.CompilerServices;
 
 namespace ABMGS.ServerV2.Grains;
-
-public interface IPlayerActor : IGrainWithGuidKey
-{
-    public Task StartGameLoop(WebSocket SocketHandle, string UniquePlayerId, CancellationToken AbnormalExitToken);
-    // public Task<INetworkReceiveActor> GetNetworkReceiveActor();
-}
-
 public class NetworkBuffer : IDisposable
 {
     private readonly Pipe _pipe;
@@ -44,6 +37,95 @@ public class NetworkBuffer : IDisposable
         _pipe.Reset();
     }
 }
+
+public class FlatBufferParser
+{
+    public T Deserialize<T>(byte[] data) where T : 
+
+}
+
+
+public interface IGameSessionActor : IGrainWithGuidKey
+{
+    public Task StartGameLoop(string uniquePlayerId, WebSocket webSocket, CancellationToken cancellationToken);
+}
+
+public class GameSessionActor : Grain, IGameSessionActor 
+{
+    private readonly ILogger<GameSessionActor> _logger;
+    private readonly IClusterClient _clusterClient;
+
+    public GameSessionActor(ILogger<GameSessionActor> logger, IClusterClient clusterClient)
+    {
+        _logger = logger;
+        _clusterClient = clusterClient;
+    }
+
+    public async Task StartGameLoop(string uniquePlayerId, WebSocket SocketObject, CancellationToken abnormalExitToken)
+    {
+        IPlayerActor playerActor = _clusterClient.GetGrain<IPlayerActor>(new Guid(uniquePlayerId));
+        FlatBufferParser parser = new FlatBufferParser();
+
+        bool IsGameLoopValid = true;
+
+        using (NetworkBuffer NBuf = new(4096))
+        {
+            //Loop to receive data from the WebSocket
+            while (IsGameLoopValid && !abnormalExitToken.IsCancellationRequested)
+            {
+                while (true)
+                {
+                    ValueWebSocketReceiveResult result = await SocketObject.ReceiveAsync(NBuf.GetReceiveBuffer(), abnormalExitToken);
+                    NBuf.AddBuffer(result.Count);
+
+                    if (result.EndOfMessage == true)
+                    {
+                        await NBuf.FinishReceived();
+                        break;
+                    }
+                }
+            }
+
+            // Get the received data
+            byte[] receivedData = await NBuf.Read();
+
+            parser.Deserialize<T>(receivedData);
+
+
+            PacketWrapper packetWrapper = PacketWrapper.GetRootAsPacketWrapper(new ByteBuffer(receivedData));
+            switch (packetWrapper.SystemPacketType)
+            {
+                case SystemPacket.LoginRequest:
+                    var loginRequest = packetWrapper.SystemPacketAsLoginRequest();
+                    _logger.LogInformation("Received LoginRequest with Username: {Username}", loginRequest.Id);
+                    break;
+                case SystemPacket.MoveRequest:
+                    MoveRequest moveRequest = packetWrapper.SystemPacketAsMoveRequest();
+                    _logger.LogInformation("Received MoveRequest with Direction: {Direction}", moveRequest.X);
+                    break;
+                default:
+                    _logger.LogWarning("Received unknown packet type: {PacketType}", packetWrapper.SystemPacketType);
+                    break;
+            }
+
+
+
+            await Task.CompletedTask;
+    }
+}
+
+public interface IFlatBufferSerializer : IGrainWithGuidKey
+{
+    public 
+}
+
+
+public interface IPlayerActor : IGrainWithGuidKey
+{
+    public Task StartGameLoop(WebSocket SocketHandle, string UniquePlayerId, CancellationToken AbnormalExitToken);
+    // public Task<INetworkReceiveActor> GetNetworkReceiveActor();
+}
+
 public class PlayerActor : Grain, IPlayerActor
 {
     private readonly ILogger<PlayerActor> _logger;
@@ -81,6 +163,7 @@ public class PlayerActor : Grain, IPlayerActor
 
             // Get the received data
             byte[] receivedData = await NBuf.Read();
+            
             PacketWrapper packetWrapper = PacketWrapper.GetRootAsPacketWrapper(new ByteBuffer(receivedData));
             switch(packetWrapper.SystemPacketType)
             {
