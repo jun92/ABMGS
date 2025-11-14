5ee621ba0144a912eb67c4c89280c27a7032e24a
Junsu Jeon <pds.jeon@gmail.com>
2025-11-14 16:39:20 +0900

    Aspire support

---
+++ Source/AppHost/AppHost.cs
@@ -0,0 +1,8 @@
+using Aspire.Hosting.Orleans;
+
+var builder = DistributedApplication.CreateBuilder(args);
+
+var redis = builder.AddRedis("redis");
+builder.AddProject<Projects.ABMGS_ServerV2>("orleans-frontend").WithReference(redis);
+
+builder.Build().Run();
