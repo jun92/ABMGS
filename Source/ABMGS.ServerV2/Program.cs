3b55f420bbeff45706182fd738c9ab6632cee131
Junsu Jeon <pds.jeon@gmail.com>
2025-11-14 16:39:20 +0900

    Aspire support

--- Source/ABMGS.ServerV2/Program.cs
+++ Source/ABMGS.ServerV2/Program.cs
@@ -17,16 +17,20 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
+builder.AddServiceDefaults();
 
 // SyncnetPlatform Actors
 builder.UseSyncnetPlatform();
 //builder.AddCustomPacketHandler<CustomPacketHandler>();
 
 // Orleans Configuration
-builder.UseOrleans(builder => {
-    builder.UseRedisClustering(options => {
-        options.ConfigurationOptions = ConfigurationOptions.Parse("localhost:6379");
+builder.UseOrleans( builder => {
+    builder.UseRedisClustering(options =>
+    {
+        options.ConfigurationOptions = ConfigurationOptions.Parse(
+            builder.Configuration.GetConnectionString("redis") ?? throw new InvalidOperationException());
     });
+    //builder.UseLocalhostClustering();
 });
 
 var app = builder.Build();
@@ -36,7 +40,7 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
-
+app.MapDefaultEndpoints();
 app.UseHttpsRedirection();
 app.UseWebSockets();
 
