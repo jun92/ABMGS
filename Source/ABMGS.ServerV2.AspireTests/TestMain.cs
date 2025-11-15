3ca8976aa7feabc21cb1f18d5b87c7aa3f30513e
Junsu Jeon <pds.jeon@gmail.com>
2025-11-15 13:45:21 +0900

    Aspire Integration Test template added

---
+++ Source/ABMGS.ServerV2.AspireTests/TestMain.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABMGS.ServerV2.AspireTest;
+
+public class TestMain
+{
+    [Fact]
+    public async Task DummyTest()
+    {
+        var builder = await DistributedApplicationTestingBuilder.CreateAsync<Projects.AppHost>();
+
+        builder.Services.ConfigureHttpClientDefaults(clientBuilder => {
+            clientBuilder.AddStandardResilienceHandler();
+        });
+
+        await using var app = await builder.BuildAsync();
+        await app.StartAsync();
+
+
+        var httpClient = app.CreateHttpClient("orleans-frontend");
+        using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(10));
+        await app.ResourceNotifications.WaitForResourceHealthyAsync("orleans-frontend", cts.Token);
+
+        var response = await httpClient.GetAsync("/healthcheck");
+
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+
+    }
+}
