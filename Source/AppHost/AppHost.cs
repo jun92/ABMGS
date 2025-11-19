c008741173826cc851542e5cef1ae4b723a01e66
Junsu Jeon <pds.jeon@gmail.com>
2025-11-19 13:39:22 +0900

    Silo replica = 2

--- Source/AppHost/AppHost.cs
+++ Source/AppHost/AppHost.cs
@@ -5,7 +5,8 @@ var builder = DistributedApplication.CreateBuilder(args);
 var redis = builder.AddRedis("redis");
 
 var silo = builder.AddProject<Projects.ABMGS_ServerV2_Silo>("silo")
-    .WithReference(redis);
+    .WithReference(redis)
+    .WithReplicas(2);
 
 builder.AddProject<Projects.ABMGS_ServerV2>("orleans-frontend")
     .WithReference(silo)
