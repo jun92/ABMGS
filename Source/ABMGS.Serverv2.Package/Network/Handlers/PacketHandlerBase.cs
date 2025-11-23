f740720723dc54bbee2b92f4ded2d833a27ef5db
Junsu Jeon <dec04th@hotmail.com>
2025-11-23 21:37:20 +0900

    Update PacketHandlerBase.cs

--- Source/ABMGS.Serverv2.Package/Network/Handlers/PacketHandlerBase.cs
+++ Source/ABMGS.Serverv2.Package/Network/Handlers/PacketHandlerBase.cs
@@ -1,31 +1,55 @@
 using Microsoft.Extensions.Logging;
 using Orleans;
+using Orleans.Services;
 using SyncnetPlatform.Interfaces.Actors;
 using SyncnetPlatform.Interfaces.Network.Sessions;
 
 namespace SyncnetPlatform.Network.Handlers;
 
+
+public class GameObjectFactoryService : IGrainService
+{
+    private readonly IGrainFactory _grainFactory;
+    public GameObjectFactoryService(IGrainFactory grainFactory)
+    {
+        _grainFactory = grainFactory;
+    }
+    public IPlayerActor GetPlayer(Guid playerGuid) => _grainFactory.GetGrain<IPlayerActor>(playerGuid);
+    public IPlayerDataActor GetPlayerData(Guid playerGuid) => _grainFactory.GetGrain<IPlayerDataActor>(playerGuid);
+    public IPlayerInventoryActor GetPlayerInventory(Guid playerGuid) => _grainFactory.GetGrain<IPlayerInventoryActor>(playerGuid);
+    public async Task SendData(Guid toPlayerId, byte[] data)
+    {
+        ISendDataGrain sendDataGrain = _grainFactory.GetGrain<ISendDataGrain>(toPlayerId);
+        await sendDataGrain.Send(data);
+    }
+}
+public class PacketContext
+{
+    public Guid PlayerId {get; set;}
+     
+}
+
 public class PacketHandlerBase
 {
     protected readonly ILogger<PacketHandlerBase> _logger;
 
-    protected readonly IClusterClient _clusterClient;
+    protected readonly IGrainFactory _grainFactory;
 
     public PacketHandlerBase(
         ILogger<PacketHandlerBase> logger,
-        IClusterClient clusterClient
+        IGrainFactory grainFactory
         )
     {
         _logger = logger;
-        _clusterClient = clusterClient;
+        _grainFactory = grainFactory;
     }
 
-    protected IPlayerActor GetPlayer(Guid playerGuid) => _clusterClient.GetGrain<IPlayerActor>(playerGuid);
-    protected IPlayerDataActor GetPlayerData(Guid playerGuid) => _clusterClient.GetGrain<IPlayerDataActor>(playerGuid);
-    protected IPlayerInventoryActor GetPlayerInventory(Guid playerGuid) => _clusterClient.GetGrain<IPlayerInventoryActor>(playerGuid);
+    protected IPlayerActor GetPlayer(Guid playerGuid) => _grainFactory.GetGrain<IPlayerActor>(playerGuid);
+    protected IPlayerDataActor GetPlayerData(Guid playerGuid) => _grainFactory.GetGrain<IPlayerDataActor>(playerGuid);
+    protected IPlayerInventoryActor GetPlayerInventory(Guid playerGuid) => _grainFactory.GetGrain<IPlayerInventoryActor>(playerGuid);
     protected async Task SendData(Guid toPlayerId, byte[] data)
     {
-        ISendDataGrain sendDataGrain = _clusterClient.GetGrain<ISendDataGrain>(toPlayerId);
+        ISendDataGrain sendDataGrain = _grainFactory.GetGrain<ISendDataGrain>(toPlayerId);
         await sendDataGrain.Send(data);
 
     }
