bc00bbe4540acc5bca243a0d4e9cbb6aa394ac23
Junsu Jeon <pds.jeon@gmail.com>
2025-11-25 13:18:18 +0900

    Sanity removal

--- Source/ABMGS.Serverv2.Package/Network/Handlers/PacketHandlerBase.cs
+++ Source/ABMGS.Serverv2.Package/Network/Handlers/PacketHandlerBase.cs
@@ -6,23 +6,6 @@ using SyncnetPlatform.Interfaces.Network.Sessions;
 
 namespace SyncnetPlatform.Network.Handlers;
 
-
-//public class GameObjectFactoryService : IGrainService
-//{
-//    private readonly IGrainFactory _grainFactory;
-//    public GameObjectFactoryService(IGrainFactory grainFactory)
-//    {
-//        _grainFactory = grainFactory;
-//    }
-//    public IPlayerActor GetPlayer(Guid playerGuid) => _grainFactory.GetGrain<IPlayerActor>(playerGuid);
-//    public IPlayerDataActor GetPlayerData(Guid playerGuid) => _grainFactory.GetGrain<IPlayerDataActor>(playerGuid);
-//    public IPlayerInventoryActor GetPlayerInventory(Guid playerGuid) => _grainFactory.GetGrain<IPlayerInventoryActor>(playerGuid);
-//    public async Task SendData(Guid toPlayerId, byte[] data)
-//    {
-//        ISendDataGrain sendDataGrain = _grainFactory.GetGrain<ISendDataGrain>(toPlayerId);
-//        await sendDataGrain.Send(data);
-//    }
-//}
 public class PacketContext
 {
     private readonly IGrainFactory _grainFactory;
@@ -43,29 +26,3 @@ public class PacketContext
         await sendDataGrain.Send(data);
     }
 }
-
-//public class PacketHandlerBase
-//{
-//    protected readonly ILogger<PacketHandlerBase> _logger;
-
-//    protected readonly IGrainFactory _grainFactory;
-
-//    public PacketHandlerBase(
-//        ILogger<PacketHandlerBase> logger,
-//        IGrainFactory grainFactory
-//        )
-//    {
-//        _logger = logger;
-//        _grainFactory = grainFactory;
-//    }
-
-//    protected IPlayerActor GetPlayer(Guid playerGuid) => _grainFactory.GetGrain<IPlayerActor>(playerGuid);
-//    protected IPlayerDataActor GetPlayerData(Guid playerGuid) => _grainFactory.GetGrain<IPlayerDataActor>(playerGuid);
-//    protected IPlayerInventoryActor GetPlayerInventory(Guid playerGuid) => _grainFactory.GetGrain<IPlayerInventoryActor>(playerGuid);
-//    protected async Task SendData(Guid toPlayerId, byte[] data)
-//    {
-//        ISendDataGrain sendDataGrain = _grainFactory.GetGrain<ISendDataGrain>(toPlayerId);
-//        await sendDataGrain.Send(data);
-
-//    }
-//}
