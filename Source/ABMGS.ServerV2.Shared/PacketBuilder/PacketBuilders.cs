5217dccd2f78c575311ecd35c6385b4b3f40463c
Junsu Jeon <pds.jeon@gmail.com>
2025-11-25 14:19:39 +0900

    Refactor packet builder classes

--- Source/ABMGS.ServerV2.Shared/PacketBuilder/PacketBuilders.cs
+++ Source/ABMGS.ServerV2.Shared/PacketBuilder/PacketBuilders.cs
@@ -3,30 +3,72 @@ using SyncnetPlatform.Protocols.Generated;
 
 namespace SyncnetPlatform.Network.Utils;
 
-internal class PongPacketBuilder : IPacketByteArrayBuilder<PongArgs>
+public abstract class PacketBABuilder<ArgsType> : IPacketByteArrayBuilder<ArgsType> where ArgsType : IPacketBuildArgs
 {
-    public byte[] Build(PongArgs args)
+    public abstract byte[] Build(ArgsType args);
+
+    public byte[] Wrap(FlatBufferBuilder builder, SystemPacket packetType, int offsetValue)
+    {
+        Offset<PacketWrapper> packetWrapperOffset = PacketWrapper.CreatePacketWrapper(
+            builder,
+            packetType,
+            offsetValue);
+        builder.Finish(packetWrapperOffset.Value);
+
+        return builder.SizedByteArray();
+    }
+}
+
+internal class PongPacketBuilder : PacketBABuilder<PongArgs>
+{
+    public override byte[] Build(PongArgs args)
     {
         FlatBufferBuilder builder = new FlatBufferBuilder(4096);
         Offset<Pong> pongOffset = Pong.CreatePong(builder, args.Seq);
-        Offset<PacketWrapper> packetWrapperOffset = PacketWrapper.CreatePacketWrapper(builder, SystemPacket.Pong, pongOffset.Value);
-        builder.Finish(packetWrapperOffset.Value);
-        
-        return builder.SizedByteArray();
+
+        return Wrap(builder, SystemPacket.Pong, pongOffset.Value);
     }
+
 }
 
-internal class PingPacketBuilder : IPacketByteArrayBuilder<PingArgs>
+internal class PingPacketBuilder : PacketBABuilder<PingArgs>
 {
-    public byte[] Build(PingArgs args)
+    public override byte[] Build(PingArgs args)
     {
         FlatBufferBuilder builder = new FlatBufferBuilder(4096);
         Offset<Ping> pingOffset = Ping.CreatePing(builder, args.Seq);
-        Offset<PacketWrapper> packetWrapperOffset = PacketWrapper.CreatePacketWrapper(
-            builder, 
-            SystemPacket.Ping, pingOffset.Value);
-        builder.Finish(packetWrapperOffset.Value);
-        return builder.SizedByteArray();
+
+        return Wrap(builder, SystemPacket.Ping, pingOffset.Value);
     }
 }
 
+internal class ReqUserInfoPacketBuilder: PacketBABuilder<ReqUserInfoArgs>
+{
+    public override byte[] Build(ReqUserInfoArgs args)
+    {
+        FlatBufferBuilder builder = new FlatBufferBuilder(4096);
+        ReqUserInfo.StartReqUserInfo(builder);
+        Offset<ReqUserInfo> offsetUserInfo = ReqUserInfo.EndReqUserInfo(builder); 
+
+        return Wrap(builder, SystemPacket.ReqUserInfo, offsetUserInfo.Value);
+    }
+}
+
+internal class ResUserInfoPacketBuilder: PacketBABuilder<ResUserInfoArgs>
+{
+    public override byte[] Build(ResUserInfoArgs args)
+    {
+        FlatBufferBuilder builder = new FlatBufferBuilder(4096);
+        ResUserInfo.StartResUserInfo(builder);
+
+        Offset<GuidType> playerId = GuidType.CreateGuidType(builder, args.playerId.ToByteArray());
+        ResUserInfo.AddPlayerId(builder, playerId);
+        StringOffset playerName = builder.CreateString(args.playerName);
+        ResUserInfo.AddPlayerName(builder, playerName);
+        ResUserInfo.AddLevel(builder, args.level);
+        ResUserInfo.AddExp(builder, args.exp);
+        Offset<ResUserInfo> offsetUserInfo = ResUserInfo.EndResUserInfo(builder);
+
+        return Wrap(builder, SystemPacket.ResUserInfo, offsetUserInfo.Value);
+    }
+}
