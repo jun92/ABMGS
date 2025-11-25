a4adbb502d54fe8101bf90fda3988659c8395501
Junsu Jeon <pds.jeon@gmail.com>
2025-11-25 15:07:26 +0900

    Add postgres database to aspire

--- Source/AppHost/AppHost.cs
+++ Source/AppHost/AppHost.cs
@@ -3,14 +3,17 @@ using Aspire.Hosting.Orleans;
 var builder = DistributedApplication.CreateBuilder(args);
 
 var redis = builder.AddRedis("redis");
+var rdbms = builder.AddPostgres("pgsql").AddDatabase("syncnet-platform");
 
 var silo = builder.AddProject<Projects.ABMGS_ServerV2_Silo>("silo")
     .WithReference(redis)
+    .WithReference(rdbms)
     .WithReplicas(2);
 
 builder.AddProject<Projects.ABMGS_ServerV2>("orleans-frontend")
     .WithReference(silo)
-    .WithReference(redis);
+    .WithReference(redis)
+    .WithReference(rdbms);
 
 
 builder.Build().Run();
