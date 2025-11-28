53930004b1474bdf8d14cb9d6bd9303cce29a7a3
Junsu Jeon <pds.jeon@gmail.com>
2025-11-28 16:45:59 +0900

    WIP

---
+++ Source/ABMGS.ServerV2.Silo/Models/PlayerModelExtend.cs
@@ -0,0 +1,44 @@
+using Microsoft.EntityFrameworkCore;
+using SyncnetPlatform.Databases;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+public interface IMakeExtendEntity
+{
+
+}
+
+public class MakeExtendEntity : IMakeExtendEntity
+{
+    public void Configure()
+    {
+
+    }
+}
+
+public class SyncnetDbContextExtend : SyncnetDbContext
+{
+
+    public DbSet<PlayerDataModelExtend> playerExtend;
+    public SyncnetDbContextExtend(DbContextOptions<SyncnetDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<PlayerDataModelExtend>().HasOne<PlayerDataModel>(p => p.PlayerDataModel);
+        base.OnModelCreating(modelBuilder);
+    }
+
+
+}
+
+[Table("players_extend")]
+public class PlayerDataModelExtend
+{
+    public int Id { get; set; }
+    public int Level { get; set; }
+    public ulong Exp { get; set; }
+    public PlayerDataModel PlayerDataModel { get; set; }
+}
+
