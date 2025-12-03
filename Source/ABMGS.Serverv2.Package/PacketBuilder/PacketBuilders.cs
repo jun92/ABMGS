0836a30d4f84ef0d8fef5c3776688ae41b2f7d98
Junsu Jeon <pds.jeon@gmail.com>
2025-12-03 14:49:40 +0900

    Update PacketBuilders.cs

--- Source/ABMGS.Serverv2.Package/PacketBuilder/PacketBuilders.cs
+++ Source/ABMGS.Serverv2.Package/PacketBuilder/PacketBuilders.cs
@@ -5,7 +5,7 @@ namespace SyncnetPlatform.Network.Utils;
 
 public abstract class PacketBABuilder<ArgsType> : IPacketByteArrayBuilder<ArgsType> where ArgsType : IPacketBuildArgs
 {
-    internal FlatBufferBuilder CreateBuilder() => new FlatBufferBuilder(4096);
+    internal FlatBufferBuilder CreateBuilder() => new (4096);
     public abstract byte[] Build(ArgsType args);
 
     public byte[] Wrap(FlatBufferBuilder builder, SystemPacket packetType, int offsetValue)
@@ -24,7 +24,8 @@ internal class PongPacketBuilder : PacketBABuilder<PongArgs>
 {
     public override byte[] Build(PongArgs args)
     {
-        FlatBufferBuilder builder = new FlatBufferBuilder(4096);
+        var builder = CreateBuilder();
+
         Offset<Pong> pongOffset = Pong.CreatePong(builder, args.Seq);
 
         return Wrap(builder, SystemPacket.Pong, pongOffset.Value);
@@ -36,7 +37,7 @@ internal class PingPacketBuilder : PacketBABuilder<PingArgs>
 {
     public override byte[] Build(PingArgs args)
     {
-        FlatBufferBuilder builder = new FlatBufferBuilder(4096);
+        var builder = CreateBuilder();
         Offset<Ping> pingOffset = Ping.CreatePing(builder, args.Seq);
 
         return Wrap(builder, SystemPacket.Ping, pingOffset.Value);
@@ -47,7 +48,7 @@ internal class ReqUserInfoPacketBuilder: PacketBABuilder<ReqUserInfoArgs>
 {
     public override byte[] Build(ReqUserInfoArgs args)
     {
-        FlatBufferBuilder builder = new FlatBufferBuilder(4096);
+        var builder = CreateBuilder();
         ReqUserInfo.StartReqUserInfo(builder);
         Offset<ReqUserInfo> offsetUserInfo = ReqUserInfo.EndReqUserInfo(builder); 
 
@@ -59,9 +60,8 @@ internal class ResUserInfoPacketBuilder: PacketBABuilder<ResUserInfoArgs>
 {
     public override byte[] Build(ResUserInfoArgs args)
     {
-        FlatBufferBuilder builder = new FlatBufferBuilder(4096);
+        var builder = CreateBuilder();
         ResUserInfo.StartResUserInfo(builder);
-
         Offset<GuidType> playerId = GuidType.CreateGuidType(builder, args.playerId.ToByteArray());
         ResUserInfo.AddPlayerId(builder, playerId);
         StringOffset playerName = builder.CreateString(args.playerName);
