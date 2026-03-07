9e827d36e4e3535582560549a383b6cc1b93983d
Junsu Jeon <dec04th@hotmail.com>
2026-03-07 21:36:21 +0900

    Move the exception class to a file

--- Source/ABMGS.Serverv2.Package/PacketBuilder/SyncnetPacketBuilder.cs
+++ Source/ABMGS.Serverv2.Package/PacketBuilder/SyncnetPacketBuilder.cs
@@ -31,10 +31,3 @@ public class SyncnetPacketBuilder
         }
     }
 }
-
-public class FlatBufferPacketBuildException : Exception
-{
-    public FlatBufferPacketBuildException(string message, Exception ex) :base(message, ex)
-    {
-    }
-}
