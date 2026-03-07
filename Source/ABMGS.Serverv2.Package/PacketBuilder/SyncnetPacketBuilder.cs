75d7617f9bb99eed5989eef196a8bf2173ab577a
Junsu Jeon <dec04th@hotmail.com>
2026-03-07 21:31:38 +0900

    Add exception handling for packet building failture

--- Source/ABMGS.Serverv2.Package/PacketBuilder/SyncnetPacketBuilder.cs
+++ Source/ABMGS.Serverv2.Package/PacketBuilder/SyncnetPacketBuilder.cs
@@ -7,22 +7,34 @@ public class SyncnetPacketBuilder
 
     public static byte[] Build<TArgs>(TArgs args) where TArgs : IPacketBuildArgs
     {
-        if(!_cache.TryGetValue(args.GetType(), out var builder))
+        try
         {
-            var type = Assembly.GetExecutingAssembly().GetTypes().FirstOrDefault(
-                    t =>
-                        t.GetInterfaces().Any(i =>
-                            i.IsGenericType &&
-                            i.GetGenericTypeDefinition() == typeof(IPacketByteArrayBuilder<>) &&
-                            i.GetGenericArguments()[0] == typeof(TArgs)
-                        )
+            if (!_cache.TryGetValue(args.GetType(), out var builder))
+            {
+                var type = Assembly.GetExecutingAssembly().GetTypes().FirstOrDefault(
+                        t =>
+                            t.GetInterfaces().Any(i =>
+                                i.IsGenericType &&
+                                i.GetGenericTypeDefinition() == typeof(IPacketByteArrayBuilder<>) &&
+                                i.GetGenericArguments()[0] == typeof(TArgs)
+                            )
 
-                ) ?? throw new MissingMethodException($"No builder found for {typeof(TArgs).Name}");
-            builder = Activator.CreateInstance(type) ?? throw new BadImageFormatException();
-            _cache.Add(args.GetType(), builder);
+                    ) ?? throw new MissingMethodException($"No builder found for {typeof(TArgs).Name}");
+                builder = Activator.CreateInstance(type) ?? throw new BadImageFormatException();
+                _cache.Add(args.GetType(), builder);
+            }
+            return ((IPacketByteArrayBuilder<TArgs>)builder).Build(args);
         }
-        return ((IPacketByteArrayBuilder<TArgs>)builder).Build(args);
+        catch (Exception ex)
+        {
+            throw new FlatBufferPacketBuildException("FlatBuffer build error", ex);
+        }
     }
 }
 
-
+public class FlatBufferPacketBuildException : Exception
+{
+    public FlatBufferPacketBuildException(string message, Exception ex) :base(message, ex)
+    {
+    }
+}
