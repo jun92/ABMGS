7e2598d33f49061e85051882c839f294c942fe9f
Junsu Jeon <pds.jeon@gmail.com>
2026-03-10 14:53:58 +0900

    Added Serializer decorator

--- Source/ABMGS.Serverv2.Package/Exceptions/FlatBufferPacketBuildException.cs
+++ Source/ABMGS.Serverv2.Package/Exceptions/FlatBufferPacketBuildException.cs
@@ -1,5 +1,6 @@
 namespace SyncnetPlatform.Exceptions;
 
+[GenerateSerializer]
 public class FlatBufferPacketBuildException : Exception
 {
     public FlatBufferPacketBuildException(string message, Exception ex) :base(message, ex)
