cd244d91b9694637a06bdeac02abf20e403ace10
Junsu Jeon <pds.jeon@gmail.com>
2026-03-26 15:38:46 +0900

    Add GenerateSerializer for all packet records

--- Source/ABMGS.Serverv2.Package/PacketBuilder/Args.cs
+++ Source/ABMGS.Serverv2.Package/PacketBuilder/Args.cs
@@ -4,30 +4,31 @@ namespace SyncnetPlatform.Network.Utils;
 
 
 // Player.fbs
-public record PingArgs(int Seq) : IPacketBuildArgs;
-public record PongArgs(int Seq) : IPacketBuildArgs;
-public record ReqUserInfoArgs(): IPacketBuildArgs;
-public record ResUserInfoArgs(Guid PlayerId, string PlayerName): IPacketBuildArgs;
-public record ReqUpdatePlayerNameArgs(string PlayerName) : IPacketBuildArgs;
-public record ResUpdatePlayerNameArgs(PacketErrorCodes Result): IPacketBuildArgs;
+[GenerateSerializer] public record PingArgs(int Seq) : IPacketBuildArgs;
+[GenerateSerializer] public record PongArgs(int Seq) : IPacketBuildArgs;
+[GenerateSerializer] public record ReqUserInfoArgs(): IPacketBuildArgs;
+[GenerateSerializer] public record ResUserInfoArgs(Guid PlayerId, string PlayerName): IPacketBuildArgs;
+[GenerateSerializer] public record ReqUpdatePlayerNameArgs(string PlayerName) : IPacketBuildArgs;
+[GenerateSerializer] public record ResUpdatePlayerNameArgs(PacketErrorCodes Result): IPacketBuildArgs;
 
-public record ReqDirectDeliveryDataArgs(Guid ToPlayerId, string Message, DirectDeliveryDataType DateType) : IPacketBuildArgs;
-public record ResDirectDeliveryDataArgs(PacketErrorCodes ErrorCode) : IPacketBuildArgs;
-public record OnDirectDeliveryDataArgs(Guid FromPlayerId, string Message, DirectDeliveryDataType DataType) : IPacketBuildArgs;
+[GenerateSerializer] public record ReqDirectDeliveryDataArgs(Guid ToPlayerId, string Message, DirectDeliveryDataType DateType) : IPacketBuildArgs;
+[GenerateSerializer] public record ResDirectDeliveryDataArgs(PacketErrorCodes ErrorCode) : IPacketBuildArgs;
+[GenerateSerializer] public record OnDirectDeliveryDataArgs(Guid FromPlayerId, string Message, DirectDeliveryDataType DataType) : IPacketBuildArgs;
 
-public record ReqCreateRoomArgs(string name, bool isPrivate = true, string password = "", int maxCount = 1) : IPacketBuildArgs;
-public record ResCreateRoomArgs(PacketErrorCodes result, Guid roomId) : IPacketBuildArgs;
+[GenerateSerializer] public record ReqCreateRoomArgs(string name, bool isPrivate = true, string password = "", int maxCount = 1) : IPacketBuildArgs;
+[GenerateSerializer] public record ResCreateRoomArgs(PacketErrorCodes result, Guid roomId) : IPacketBuildArgs;
 
-public record ReqJoinRoomArgs(Guid roomId, string password) : IPacketBuildArgs;
-public record ResJoinRoomArgs(PacketErrorCodes result): IPacketBuildArgs;
-public record OnPlayerJoinRoomArgs(Guid roomId, Guid playerId, string playerName): IPacketBuildArgs;
+[GenerateSerializer] public record ReqJoinRoomArgs(Guid roomId, string password) : IPacketBuildArgs;
+[GenerateSerializer] public record ResJoinRoomArgs(PacketErrorCodes result): IPacketBuildArgs;
+[GenerateSerializer] public record OnPlayerJoinRoomArgs(Guid roomId, Guid playerId, string playerName): IPacketBuildArgs;
 
-public record ReqLeaveRoomArgs(Guid roomId) : IPacketBuildArgs;
-public record ResLeaveRoomArgs(PacketErrorCodes result) : IPacketBuildArgs;
+[GenerateSerializer] public record ReqLeaveRoomArgs(Guid roomId) : IPacketBuildArgs;
+[GenerateSerializer] public record ResLeaveRoomArgs(PacketErrorCodes result) : IPacketBuildArgs;
+[GenerateSerializer] public record OnPlayerLeaveRoomArgs(Guid roomId, Guid playerId): IPacketBuildArgs;
 
-public record ReqBroadcastRoomArgs(Guid roomId, Guid from, string message) : IPacketBuildArgs;
-public record ResBroadcastRoomArgs(PacketErrorCodes result) : IPacketBuildArgs;
-public record BroadcastRoomArgs(Guid from, string message) : IPacketBuildArgs;
+[GenerateSerializer] public record ReqBroadcastRoomArgs(Guid roomId, Guid from, string message) : IPacketBuildArgs;
+[GenerateSerializer] public record ResBroadcastRoomArgs(PacketErrorCodes result) : IPacketBuildArgs;
+[GenerateSerializer] public record BroadcastRoomArgs(Guid from, string message) : IPacketBuildArgs;
 
 
 
