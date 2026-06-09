d8fd16f9da18118b3f500afa5215e6327b634c0d
Junsu Jeon <pds.jeon@gmail.com>
2026-06-09 14:33:54 +0900

    Removed serviceDefault.csproj dependency

--- Source/ABMGS.Serverv2.Package/Extensions/SyncnetPlatformBuilderExtension.cs
+++ Source/ABMGS.Serverv2.Package/Extensions/SyncnetPlatformBuilderExtension.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.Extensions.ServiceDiscovery;
 using Microsoft.IdentityModel.Tokens;
 using OpenTelemetry.Logs;
 using OpenTelemetry.Metrics;
