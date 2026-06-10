d73a299147700883cf0fdb4b821e08bd5c8a60f1
Junsu Jeon <pds.jeon@gmail.com>
2026-06-10 14:22:46 +0900

    Actor Interface moved

---
+++ Source/Package/Interfaces/Actors/IPlayRoomActor.cs
@@ -0,0 +1,14 @@
+using SyncnetPlatform.Actors;
+using SyncnetPlatform.Protocols.Generated;
+
+namespace SyncnetPlatform.Interfaces.Actors;
+
+public interface IPlayRoomActor : IGrainWithGuidKey
+{
+    Task<List<PlayRoomMember>> GetPlayersInPlayRoom();
+    Task<bool> IsValidRoomToJoin();
+    Task<PacketErrorCodes> JoinPlayer(PlayRoomMember joiner);
+    Task<PacketErrorCodes> LeavePlayer(PlayRoomMember leaver);
+    Task SetRoomInformation(string displayName, bool isPrivate, int maxCapacity, string roomPassword, PlayRoomMember owner);
+}
+
