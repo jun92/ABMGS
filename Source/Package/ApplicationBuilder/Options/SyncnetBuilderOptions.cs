bee1640266c6da334ac9cbc93a4ba1517aaabf58
Junsu Jeon <pds.jeon@gmail.com>
2026-06-21 14:28:50 +0900

    Create SyncnetBuilderOptions.cs

---
+++ Source/Package/ApplicationBuilder/Options/SyncnetBuilderOptions.cs
@@ -0,0 +1,23 @@
+using SyncnetPlatform.Actors;
+using SyncnetPlatform.Databases;
+using SyncnetPlatform.Extensions.Options;
+
+namespace SyncnetPlatform.ApplicationBuilder.Options;
+
+public class SyncnetBuilderOptions
+{
+    public Action<SyncnetTelemetryOption>? TelemetryConfigure { get; set; } = null;
+    public Action<SyncnetLoggerOption>? LoggerConfigure { get; set; } = null;
+    public bool AutoMigrateDatabase { get; set; } = false;
+
+    public Type? PlayerDataExtendCreateType { get; private set; } = null;
+    public Type? PlayerCustomBehaviorType { get; private set; } = null;
+    public void UsePlayerDataExtendCreator<T>() where T: class, IPlayerDataExtendCreater
+    {
+        PlayerDataExtendCreateType = typeof(T);
+    }
+    public void UsePlayerCustomBehavior<T>() where T: class, IPlayerCustomBehavior
+    {
+        PlayerCustomBehaviorType = typeof(T);
+    }
+}
