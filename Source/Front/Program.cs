49de0b73b5a913d54f671f8233544d6de2baa0e5
Junsu Jeon <pds.jeon@gmail.com>
2026-06-21 14:24:12 +0900

    applied new Applciation builder classes

--- Source/Front/Program.cs
+++ Source/Front/Program.cs
@@ -3,56 +3,94 @@ using SyncnetPlatform.Extensions;
 using SyncnetPlatform.Extensions.Options;
 using OpenTelemetry.Exporter;
 
-var builder = WebApplication.CreateBuilder(args);
 
+var builder = SyncnetApplicationBuilder.CreateFrontBuilder(args);
 
-// if want to use external telemetry services, for using default aspire dashboard, should be null.
-var IsSpecificTelemetryEndpoints = builder.Configuration.GetSection("ConnectionStrings:telemetry").Exists();
-
-Action<SyncnetTelemetryOption> telemetryConfigure = option =>
+builder.ConfigureFront(options =>
 {
-    option.Logging.Endpoint = builder.Configuration.GetValue<string>("ConnectionStrings:telemetry:logging:EndPoint")!;
-    string protocol = builder.Configuration.GetValue<string>("ConnectionStrings:telemetry:logging:Protocol")!;
-    if (Enum.TryParse<OtlpExportProtocol>(protocol, ignoreCase: false, out var outProtocol))
+    if (builder.Configuration.GetSection("ConnectionStrings:telemetry").Exists())
     {
-        option.Logging.Protocol = outProtocol;
+        Action<SyncnetTelemetryOption> telemetryConfigure = option =>
+        {
+            option.Logging.Endpoint = builder.Configuration.GetValue<string>("ConnectionStrings:telemetry:logging:EndPoint")!;
+            string protocol = builder.Configuration.GetValue<string>("ConnectionStrings:telemetry:logging:Protocol")!;
+            if (Enum.TryParse<OtlpExportProtocol>(protocol, ignoreCase: false, out var outProtocol))
+            {
+                option.Logging.Protocol = outProtocol;
+            }
+            option.Metric.Endpoint = builder.Configuration.GetValue<string>("ConnectionStrings:telemetry:metric:EndPoint")!;
+            protocol = builder.Configuration.GetValue<string>("ConnectionStrings:telemetry:metric:Protocol")!;
+            if (Enum.TryParse<OtlpExportProtocol>(protocol, ignoreCase: false, out outProtocol))
+            {
+                option.Metric.Protocol = outProtocol;
+            }
+            option.Trace.Endpoint = builder.Configuration.GetValue<string>("ConnectionStrings:telemetry:trace:EndPoint")!;
+            protocol = builder.Configuration.GetValue<string>("ConnectionStrings:telemetry:trace:Protocol")!;
+            if (Enum.TryParse<OtlpExportProtocol>(protocol, ignoreCase: false, out outProtocol))
+            {
+                option.Trace.Protocol = outProtocol;
+            }
+        };
+        options.TelemetryConfigure = telemetryConfigure; 
     }
-    option.Metric.Endpoint = builder.Configuration.GetValue<string>("ConnectionStrings:telemetry:metric:EndPoint")!;
-    protocol = builder.Configuration.GetValue<string>("ConnectionStrings:telemetry:metric:Protocol")!;
-    if (Enum.TryParse<OtlpExportProtocol>(protocol, ignoreCase: false, out outProtocol))
-    {
-        option.Metric.Protocol = outProtocol;
-    }
-    option.Trace.Endpoint = builder.Configuration.GetValue<string>("ConnectionStrings:telemetry:trace:EndPoint")!;
-    protocol = builder.Configuration.GetValue<string>("ConnectionStrings:telemetry:trace:Protocol")!;
-    if (Enum.TryParse<OtlpExportProtocol>(protocol, ignoreCase: false, out outProtocol))
-    {
-        option.Trace.Protocol = outProtocol;
-    }
-};
-IsSpecificTelemetryEndpoints = false;
-builder.AddSyncnetPlatformClient(TelemetryAction: IsSpecificTelemetryEndpoints ? telemetryConfigure: null);
+});
 
-builder.Services.AddControllers();
+var SyncnetFrontApp = builder.Build();
 
-var app = builder.Build();
+await SyncnetFrontApp.RunAsync();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+//var builder = WebApplication.CreateBuilder(args);
 
-app.UseRouting();
-app.UseAuthentication();
-app.UseAuthorization();
-app.UseFrontendSyncnetPlatform();
 
-app.MapControllers();
-//app.UseEndpoints(endpoints =>
+//// if want to use external telemetry services, for using default aspire dashboard, should be null.
+//var IsSpecificTelemetryEndpoints = builder.Configuration.GetSection("ConnectionStrings:telemetry").Exists();
+
+//Action<SyncnetTelemetryOption> telemetryConfigure = option =>
 //{
-//    endpoints.MapControllers();
-//});
+//    option.Logging.Endpoint = builder.Configuration.GetValue<string>("ConnectionStrings:telemetry:logging:EndPoint")!;
+//    string protocol = builder.Configuration.GetValue<string>("ConnectionStrings:telemetry:logging:Protocol")!;
+//    if (Enum.TryParse<OtlpExportProtocol>(protocol, ignoreCase: false, out var outProtocol))
+//    {
+//        option.Logging.Protocol = outProtocol;
+//    }
+//    option.Metric.Endpoint = builder.Configuration.GetValue<string>("ConnectionStrings:telemetry:metric:EndPoint")!;
+//    protocol = builder.Configuration.GetValue<string>("ConnectionStrings:telemetry:metric:Protocol")!;
+//    if (Enum.TryParse<OtlpExportProtocol>(protocol, ignoreCase: false, out outProtocol))
+//    {
+//        option.Metric.Protocol = outProtocol;
+//    }
+//    option.Trace.Endpoint = builder.Configuration.GetValue<string>("ConnectionStrings:telemetry:trace:EndPoint")!;
+//    protocol = builder.Configuration.GetValue<string>("ConnectionStrings:telemetry:trace:Protocol")!;
+//    if (Enum.TryParse<OtlpExportProtocol>(protocol, ignoreCase: false, out outProtocol))
+//    {
+//        option.Trace.Protocol = outProtocol;
+//    }
+//};
+//IsSpecificTelemetryEndpoints = false;
+//builder.AddSyncnetPlatformClient(TelemetryAction: IsSpecificTelemetryEndpoints ? telemetryConfigure: null);
 
-await app.RunAsync();
+//builder.Services.AddControllers();
+
+//var app = builder.Build();
+
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.MapOpenApi();
+//}
+
+//app.UseRouting();
+//app.UseAuthentication();
+//app.UseAuthorization();
+//app.UseFrontendSyncnetPlatform();
+
+//app.MapControllers();
+////app.UseEndpoints(endpoints =>
+////{
+////    endpoints.MapControllers();
+////});
+
+//await app.RunAsync();
+
+
 
