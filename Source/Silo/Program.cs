5d01ed3ee936a4286c9f04b6ff6cc97f2f0ea180
Junsu Jeon <pds.jeon@gmail.com>
2026-06-21 14:24:12 +0900

    applied new Applciation builder classes

--- Source/Silo/Program.cs
+++ Source/Silo/Program.cs
@@ -6,54 +6,96 @@ using SyncnetPlatform.Extensions.Options;
 
 string? EnvironmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
 
-var builder = WebApplication.CreateBuilder(args);
 
-builder.Configuration
-    .AddCommandLine(args)
-    .SetBasePath(Directory.GetCurrentDirectory())
-    .AddJsonFile("appsettings.json", false, true)
-    .AddJsonFile($"appsettings.{EnvironmentName}.json", true, true)
-    .AddEnvironmentVariables();
-
-
-// Define custom player data per games.
-builder.Services.AddTransient<IPlayerDataExtendCreater, MyGamePlayerDataExtendCreater>();
-builder.Services.AddTransient<IPlayerCustomBehavior, MyPlayerBehavior>();
-
-
-var IsSpecificTelemetryEndpoints = builder.Configuration.GetSection("ConnectionStrings:telemetry").Exists();
-
-
-Action<SyncnetTelemetryOption> telemetryConfigure = option =>
+var builder = SyncnetApplicationBuilder.CreateActorBuilder(args);
+builder.ConfigureActor(option =>
 {
-    option.Logging.Endpoint = builder.Configuration.GetValue<string>("ConnectionStrings:telemetry:logging:Endpoint")!;
-    string protocol = builder.Configuration.GetValue<string>("ConnectionStrings:telemetry:logging:Protocol")!;
-    if (Enum.TryParse<OtlpExportProtocol>(protocol, ignoreCase: false, out var outProtocol))
+    option.UsePlayerCustomBehavior<MyPlayerBehavior>();
+    option.UsePlayerDataExtendCreator<MyGamePlayerDataExtendCreater>();
+    option.AutoMigrateDatabase = true;
+
+    if( builder.Configuration.GetSection("ConnectionStrings:telemetry").Exists())
     {
-        option.Logging.Protocol = outProtocol;
+        Action<SyncnetTelemetryOption> telemetryConfigure = option =>
+        {
+            option.Logging.Endpoint = builder.Configuration.GetValue<string>("ConnectionStrings:telemetry:logging:Endpoint")!;
+            string protocol = builder.Configuration.GetValue<string>("ConnectionStrings:telemetry:logging:Protocol")!;
+            if (Enum.TryParse<OtlpExportProtocol>(protocol, ignoreCase: false, out var outProtocol))
+            {
+                option.Logging.Protocol = outProtocol;
+            }
+            option.Metric.Endpoint = builder.Configuration.GetValue<string>("ConnectionStrings:telemetry:metric:Endpoint")!;
+            protocol = builder.Configuration.GetValue<string>("ConnectionStrings:telemetry:metric:Protocol")!;
+            if (Enum.TryParse<OtlpExportProtocol>(protocol, ignoreCase: false, out outProtocol))
+            {
+                option.Metric.Protocol = outProtocol;
+            }
+            option.Trace.Endpoint = builder.Configuration.GetValue<string>("ConnectionStrings:telemetry:trace:Endpoint")!;
+            protocol = builder.Configuration.GetValue<string>("ConnectionStrings:telemetry:trace:Protocol")!;
+            if (Enum.TryParse<OtlpExportProtocol>(protocol, ignoreCase: false, out outProtocol))
+            {
+                option.Trace.Protocol = outProtocol;
+            }
+        };
+        option.TelemetryConfigure = telemetryConfigure;
     }
-    option.Metric.Endpoint = builder.Configuration.GetValue<string>("ConnectionStrings:telemetry:metric:Endpoint")!;
-    protocol = builder.Configuration.GetValue<string>("ConnectionStrings:telemetry:metric:Protocol")!;
-    if (Enum.TryParse<OtlpExportProtocol>(protocol, ignoreCase: false, out outProtocol))
-    {
-        option.Metric.Protocol = outProtocol;
-    }
-    option.Trace.Endpoint = builder.Configuration.GetValue<string>("ConnectionStrings:telemetry:trace:Endpoint")!;
-    protocol = builder.Configuration.GetValue<string>("ConnectionStrings:telemetry:trace:Protocol")!;
-    if (Enum.TryParse<OtlpExportProtocol>(protocol, ignoreCase: false, out outProtocol))
-    {
-        option.Trace.Protocol = outProtocol;
-    }
-};
-IsSpecificTelemetryEndpoints = false;
-builder.AddSyncnetPlatformSilo(TelemetryAction:  IsSpecificTelemetryEndpoints ? telemetryConfigure : null);
-//builder.AddSyncnetPlatformSilo();
+});
 
-
-var host = builder.Build();
-host.SyncnetDbMigrate();
-await host.RunAsync();
+var SyncnetActorApp = builder.Build();
+await SyncnetActorApp.RunAsync();
 
 
 
+
+///================== initializing in old way
+//var builder = WebApplication.CreateBuilder(args);
+
+//builder.Configuration
+//    .AddCommandLine(args)
+//    .SetBasePath(Directory.GetCurrentDirectory())
+//    .AddJsonFile("appsettings.json", false, true)
+//    .AddJsonFile($"appsettings.{EnvironmentName}.json", true, true)
+//    .AddEnvironmentVariables();
+
+
+//// Define custom player data per games.
+//builder.Services.AddTransient<IPlayerDataExtendCreater, MyGamePlayerDataExtendCreater>();
+//builder.Services.AddTransient<IPlayerCustomBehavior, MyPlayerBehavior>();
+
+
+//var IsSpecificTelemetryEndpoints = builder.Configuration.GetSection("ConnectionStrings:telemetry").Exists();
+
+
+//Action<SyncnetTelemetryOption> telemetryConfigure = option =>
+//{
+//    option.Logging.Endpoint = builder.Configuration.GetValue<string>("ConnectionStrings:telemetry:logging:Endpoint")!;
+//    string protocol = builder.Configuration.GetValue<string>("ConnectionStrings:telemetry:logging:Protocol")!;
+//    if (Enum.TryParse<OtlpExportProtocol>(protocol, ignoreCase: false, out var outProtocol))
+//    {
+//        option.Logging.Protocol = outProtocol;
+//    }
+//    option.Metric.Endpoint = builder.Configuration.GetValue<string>("ConnectionStrings:telemetry:metric:Endpoint")!;
+//    protocol = builder.Configuration.GetValue<string>("ConnectionStrings:telemetry:metric:Protocol")!;
+//    if (Enum.TryParse<OtlpExportProtocol>(protocol, ignoreCase: false, out outProtocol))
+//    {
+//        option.Metric.Protocol = outProtocol;
+//    }
+//    option.Trace.Endpoint = builder.Configuration.GetValue<string>("ConnectionStrings:telemetry:trace:Endpoint")!;
+//    protocol = builder.Configuration.GetValue<string>("ConnectionStrings:telemetry:trace:Protocol")!;
+//    if (Enum.TryParse<OtlpExportProtocol>(protocol, ignoreCase: false, out outProtocol))
+//    {
+//        option.Trace.Protocol = outProtocol;
+//    }
+//};
+//IsSpecificTelemetryEndpoints = false;
+//builder.AddSyncnetPlatformSilo(TelemetryAction:  IsSpecificTelemetryEndpoints ? telemetryConfigure : null);
+////builder.AddSyncnetPlatformSilo();
+
+
+//var host = builder.Build();
+//host.SyncnetDbMigrate();
+//await host.RunAsync();
+
+
+//===========================================================================
 
