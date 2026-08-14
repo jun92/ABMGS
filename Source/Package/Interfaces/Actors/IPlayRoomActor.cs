1556c2a9c0d51b13390cfac010b4e6fae6af0e96
Junsu Jeon <dec04th@hotmail.com>
2026-08-14 13:04:39 +0900

    Added alias to PlayRoomActor

--- Source/Package/Interfaces/Actors/IPlayRoomActor.cs
+++ Source/Package/Interfaces/Actors/IPlayRoomActor.cs
@@ -3,13 +3,20 @@ using SyncnetPlatform.Protocols.Generated;
 
 namespace SyncnetPlatform.Interfaces.Actors;
 
+[Alias("SyncnetPlatform.Interfaces.Actors.IPlayRoomActor")]
 public interface IPlayRoomActor : IGrainWithGuidKey
 {
+    [Alias("GetPlayersInPlayRoom")]
     Task<List<PlayRoomMember>> GetPlayersInPlayRoom();
+    [Alias("IsValidRoomToJoin")]
     Task<bool> IsValidRoomToJoin();
+    [Alias("JoinPlayer")]
     Task<(PacketErrorCodes, byte[])> JoinPlayer(PlayRoomMember joiner);
+    [Alias("LeavePlayer")]
     Task<PacketErrorCodes> LeavePlayer(PlayRoomMember leaver);
+    [Alias("SetRoomInformation")]
     Task<(PacketErrorCodes, byte[]?)> SetRoomInformation(string displayName, bool isPrivate, int maxCapacity, string roomPassword, PlayRoomMember owner);
+    [Alias("OnPlayerActionToPlayRoom")]
     Task OnPlayerActionToPlayRoom(Guid playerId, string actionType, byte[] actionParameter);
 }
 
