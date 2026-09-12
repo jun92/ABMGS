c7604cc2cc79af429a1b13f8f7c840b51da980c8
Junsu Jeon <dec04th@hotmail.com>
2026-09-12 15:01:00 +0900

    Update PlayRoomMember.cs

--- Source/Package/Actors/PlayRoomMember.cs
+++ Source/Package/Actors/PlayRoomMember.cs
@@ -3,16 +3,10 @@ namespace SyncnetPlatform.Actors;
 [GenerateSerializer] 
 public class PlayRoomMember(Guid roomId, Guid playerId, string playerName, byte[]? playerExtendData)
 {
-    [Id(0)]
-    public Guid RoomId { get; set; } = roomId;
-
-    [Id(1)]
-    public Guid PlayerId { get; set; } = playerId;
-
-    [Id(2)]
-    public string PlayerName { get; set; } = playerName;
-
+    [Id(0)] public Guid RoomId { get; set; } = roomId;
+    [Id(1)] public Guid PlayerId { get; set; } = playerId;
+    [Id(2)] public string PlayerName { get; set; } = playerName;
     // One time use only.
-    [Id(3)]
-    public byte[]? PlayerExtendData { get; set; } = playerExtendData;
+    [Id(3)] public byte[]? PlayerExtendData { get; set; } = playerExtendData;
+    //[Id(4)] public byte[] PlayerStateForPlayRoom { get; set; } = [];
 }
