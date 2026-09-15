d7f3ecf7b33b0d27e7a168ef2212fa43976e6b20
Junsu Jeon <dec04th@hotmail.com>
2026-09-15 14:39:36 +0900

    Added migration for tictactoe schema

---
+++ Source/Silo/Migrations/20260915053000_tictactoe-stats.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Silo.Migrations
+{
+    /// <inheritdoc />
+    public partial class tictactoestats : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CustomExp",
+                table: "player_data");
+
+            migrationBuilder.DropColumn(
+                name: "CustomLevel",
+                table: "player_data");
+
+            migrationBuilder.AddColumn<int>(
+                name: "LoseCount",
+                table: "player_data",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PlayCount",
+                table: "player_data",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "WinCount",
+                table: "player_data",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LoseCount",
+                table: "player_data");
+
+            migrationBuilder.DropColumn(
+                name: "PlayCount",
+                table: "player_data");
+
+            migrationBuilder.DropColumn(
+                name: "WinCount",
+                table: "player_data");
+
+            migrationBuilder.AddColumn<long>(
+                name: "CustomExp",
+                table: "player_data",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 33L);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CustomLevel",
+                table: "player_data",
+                type: "integer",
+                nullable: false,
+                defaultValue: 1);
+        }
+    }
+}
