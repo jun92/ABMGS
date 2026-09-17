e162b621de8e6ef7889efd75d72107bb69ce581a
Junsu Jeon <dec04th@hotmail.com>
2026-09-17 14:17:41 +0900

    Custom Packet builder added

--- Source/Tests/TttGamePacketBuilder.cs
+++ Source/Tests/TttGamePacketBuilder.cs
@@ -1,8 +1,23 @@
+using Google.FlatBuffers;
+using SyncnetPlatform.Extensions;
+using TGame.Packets;
+
 namespace SyncnetPlatform.Tests;
 
 public partial class ABMGS_TestMain
 {
-    
-    // private static byte[] Build
+    private static byte[] BuildTGameReqActionSetReady(Guid playerId, bool readyState)
+    {
+        FlatBufferBuilder builder = new(1024);
+
+        StringOffset playerIdOffset = builder.CreateString(playerId.ToString());
+        
+        TGameReqActionSetReady.StartTGameReqActionSetReady(builder);
+        TGameReqActionSetReady.AddPlayerId(builder, playerIdOffset);
+        TGameReqActionSetReady.AddReadyState(builder, readyState);
+        Offset<TGameReqActionSetReady> offset = TGameReqActionSetReady.EndTGameReqActionSetReady(builder);
+        builder.Finish(offset.Value);
+        return builder.SizedByteArray();
+    }
     
 }
